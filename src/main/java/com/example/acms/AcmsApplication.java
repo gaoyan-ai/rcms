ca7a2b1d68c8db12f7ff46267eb31bdcd551c2ae
Administrator <1277511997@qq.com>
2021-04-03 16:47:50 +0800

    nishitiaochungou

--- src/main/java/com/example/acms/AcmsApplication.java
+++ src/main/java/com/example/acms/AcmsApplication.java
@@ -8,6 +8,8 @@ public class AcmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcmsApplication.class, args);
+        System.out.println("nishitiaochungou");
+        
     }
 
 }
